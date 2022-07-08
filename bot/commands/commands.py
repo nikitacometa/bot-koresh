ad98a2f03398f36e4dae945c798cf2b93af26a48
wackloner <wackloner@gmail.com>
2022-07-08 18:37:53 +0300

    minor help change

--- bot/commands/commands.py
+++ bot/commands/commands.py
@@ -30,6 +30,8 @@ def show_help(update: Update, context: CallbackContext):
     msg += 'Теперь ты можешь бесцельно развлекать себя и своих кожаных собратьев разными странными способами:\n'
     msg += '\n'
     msg += Commands.help_string()
+    msg += '\n'
+    msg += 'И прошу не обижаться на мои базары, я просто делаю свою работу.'
 
     update.message.reply_html(msg, disable_web_page_preview=True)
 
@@ -44,7 +46,7 @@ class Commands:
 
         # TODO: change to check + button for tracking
         Command('track', track_address_handler, help=
-                f'/track $addr – понаблюдаю за адресом и сразу отпишу, как только на последней/новой транзакции '
+                f'/track <b>$addr</b> – понаблюдаю за адресом и сразу отпишу, как только на последней/новой транзакции '
                 f'наберётся нужное количество подтверждений (по дефолту 2)'),
 
         Command('show_tracked', show_tracked, help=
@@ -54,10 +56,10 @@ class Commands:
         Separator('\n<b>Для тех, у кого есть друзья</b>'),
 
         Command('split_teams', split_into_teams, help=
-                f'/split_teams – поделить список людей на n команд (2 по дефолту)'),
+                f'/split_teams – поделить список людей на n команд (2 по дефолту), каждое имя на отдельной строке'),
 
         Command('challenge', handle_challenge, additional_dispatcher_update=challenge_update_dispatcher, help=
-                f'/challenge $t – скинуть в чат кнопку "кто быстрее нажмёт" на t ({TIME_TAGS_EXAMPLE}) времени, '
+                f'/challenge <b>$t</b> – скинуть в чат кнопку "кто быстрее нажмёт" на $t ({TIME_TAGS_EXAMPLE}) времени, '
                 f'если вдруг надо набрать поскорее сколько-то людей (опционально добавляем текст)'),
 
 
@@ -65,7 +67,7 @@ class Commands:
 
         # TODO: don't write name in help (add it in Command init)
         Command('show_map', show_map,
-                help=f'/show_map x,y – показать кусок карты по данным координатам'),
+                help=f'/show_map <b>$x, $y</b> – показать кусок карты по данным координатам'),
 
         Command('joke', joke, help=f'/joke – расскажу один из анеков своего деда'),
 
@@ -73,10 +75,10 @@ class Commands:
         Separator('\n<b>Chat settings</b>'),
 
         Command('troll_mode', troll_mode,
-                help=f'/troll_mode on/off – я конечно не GPT, но матом покрою от души'),
+                help=f'/troll_mode <b>on/off</b> – я конечно не GPT, но матом покрою от души'),
 
         Command('vanish_mode', vanish_mode,
-                help=f'/vanish_mode $t/off – удалять все сообщения в чате через t ({TIME_TAGS_EXAMPLE})'),
+                help=f'/vanish_mode <b>$t/off</b> – удалять все сообщения в чате через t ({TIME_TAGS_EXAMPLE})'),
 
         # something
 
