c3507dc0a650fa0c3adb70d9ed686f04b3ef82e1
wackloner <wackloner@gmail.com>
2022-07-08 15:09:09 +0300

    fix tor script

--- scripts/init_tor.sh
+++ scripts/init_tor.sh
@@ -4,13 +4,12 @@
 tor_instances=$(ps -e | grep " tor" -c)
 if (( tor_instances == 0 ))
 then
-  echo "Tor isn't running, launching..." && sudo -u wackloner tor &>/dev/null & disown
+#  echo "Tor isn't running, launching..." && sudo -u $(whoami) tor &>/dev/null & disown
+  echo "Tor isn't running, launching..." && sudo -u $(whoami) tor & disown
 
   # TODO: make timeout bigger, but chech bootstrap status and exit on done
   echo "Waiting 60 seconds for tor to bootstrap..." && sleep 60
 fi
-
-# TODO: wait for tor to bootstrap, wait, exit on timeout
 
 tor_instances=$(ps -e | grep " tor" -c)
 if (( tor_instances == 0 ))
